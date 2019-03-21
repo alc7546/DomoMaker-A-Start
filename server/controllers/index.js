e359babc3b633532f9e21bd189bac78dc019233c
Andrew Clark <alc7546@rit.edu>
2019-03-21 01:23:34 -0400

    DomoMaker A

---
+++ server/controllers/index.js
@@ -0,0 +1,4 @@
+// file gets automaticcaly imported when the folder is imported
+// pull these into public scope
+module.exports.Account = require('./Account.js');
+module.exports.Domo = require('./Domo.js');
